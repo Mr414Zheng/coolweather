8dc95bd503e2185dfaf150a8cef0cd76916f6445
Zheng <1>
2018-03-08 20:32:11 +0800

    背景完成

--- app/src/main/java/com/zhenghuaizhi/coolweather/util/Utility.java
+++ app/src/main/java/com/zhenghuaizhi/coolweather/util/Utility.java
@@ -2,9 +2,11 @@ package com.zhenghuaizhi.coolweather.util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
 import com.zhenghuaizhi.coolweather.db.City;
 import com.zhenghuaizhi.coolweather.db.County;
 import com.zhenghuaizhi.coolweather.db.Province;
+import com.zhenghuaizhi.coolweather.gson.Weather;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -79,5 +81,20 @@ public class Utility {
             return true;
         }
         return false;
+    }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
